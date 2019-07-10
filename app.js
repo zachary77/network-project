4eed396b6884bd134d611c663d5d1fe03215ab15
zachary77 <leejinhyurk@gmail.com>
2019-07-10 10:47:14 +0900

    login,register,intro page added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const assert = require('assert');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var expressErrorHandler = require('express-error-handler');
+const crypto = require('crypto');
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const dbName = 'test';
@@ -22,50 +23,77 @@ MongoClient.connect(url, function(err, client) {
 
 var app = express();
 
-app.use(favicon(path.join(__dirname, './public', 'travel_icon.ico')));
+app.use(favicon(path.join(__dirname, '/public', 'travel_icon.ico')));
 app.use(logger('dev'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+app.set('views','./views');
 
 //쿠키 미들웨어 등록
 app.use(cookieParser());
 
 //세션 환경 세팅
-//세션은 서버쪽에 저장하는 것을 말하는데, 파일로 저장 할 수도 있고 레디스라고 하는 메모리DB등 다양한 저장소에 저장 할 수가 있는데
 app.use(session({
-    secret: 'networkprogramming',   
-    resave: false,
-    saveUninitialized: true
+    key : 'sid', //세션 키
+    secret: 'secret',   //세션 암호 키(쿠키값 변조를 막기 위한 암호화)
+    resave: false,      //세션 항상 저장할지
+    saveUninitialized: true,     //세션이 저장되기 전 상태를 지정하는 부분
+    cookie: {        //쿠키 유효시간
+        maxAge: 24000 * 60 * 60 //쿠기 유효시간 24시간
+    }
 }));
 //secret – 쿠키를 임의로 변조하는것을 방지하기 위한 값 입니다. 이 값을 통하여 세션을 암호화 하여 저장합니다.
 //resave – 세션을 언제나 저장할 지 (변경되지 않아도) 정하는 값입니다. express-session documentation에서는 이 값을 false 로 하는것을 권장하고 필요에 따라 true로 설정합니다.
 //saveUninitialized – 세션이 저장되기 전에 uninitialized 상태로 미리 만들어서 저장합니다.
 
 app.get('/', (req,res) => {
-    res.sendfile('./public/travel.html');
+    res.render('index', {session:req.session, cookie:req.cookies});
 });
 
-app.post('/', (req,res) => {
+app.get('/intro', (req,res) => {
+    res.render('intro', {session:req.session, cookie:req.cookies});
+});
+
+app.get('/login',function(req,res,next){
+    let session = req.session;
+    res.render("login",{
+        session : session
+    });
+});
+
+app.get('/join', (req,res) => {
+    res.render('join');
+});
+
+app.post('/join', (req,res) => {
     if(req.body.pw != req.body.check){
-        res.send('<script type="text/javascript">alert("재입력한 비밀번호가 일치하지 않습니다.");</script>');
+        res.send('<script type="text/javascript">alert("재입력한 비밀번호가 일치하지 않습니다.");</script><script type="text/javascript">window.location="http://localhost:8080";</script>');
+        return;
     }
     if(req.body.username && req.body.id && req.body.pw && req.body.check){
-        var result = db.users.find({ id: `${req.body.username}`});
-        
+        var result = db.users.find({ id: `${req.body.id}` });
         result.toArray(function(err,docs){
             if(err){
-                console.log(err.message)
+                res.sendStatus(500);
+                console.log(err.message);
+                return;
             }
             if(docs.length > 0){
-                res.send('<script type="text/javascript">alert("이미 존재하는 아이디입니다.");</script>');
+                res.send('<script type="text/javascript">alert("이미 존재하는 아이디입니다.");</script><script type="text/javascript">window.location="http://localhost:8080";</script>');
             }else {
                 db.users.insertOne({username: `${req.body.username}`, id: `${req.body.id}`, pw: `${req.body.pw}`},
                 function(err,result){
                     if(err){
                         console.log(err.message);
-                    }else{
+                    }
+                    if(result.insertedCount > 0){
                         console.log('사용자 추가 됨');
-                        res.send('<script type="text/javascript">alert("회원가입이 완료 되었습니다.");</script>');
+                        res.send('<script type="text/javascript">alert("회원가입에 성공하였습니다.");</script><script type="text/javascript">window.location="http://localhost:8080";</script>');
+                    }else{
+                        console.log('사용자 추가 안됨');
+                        res.send('<script type="text/javascript">alert("회원가입에 실패하였습니다.");</script><script type="text/javascript">window.location="http://localhost:8080";</script>');
                     }
                 });
             }
@@ -74,10 +102,34 @@ app.post('/', (req,res) => {
 });
 
 app.post('/login', (req,res) => {
-    if(req.body.id && req.body.pw){
-        
-    }
-    res.redirect('/');
+        let body = req.body;
+        var sec = db.users.find({ id: `${req.body.id}` , pw: `${req.body.pw}`});
+        sec.toArray(function(err,docs){
+            if(err){
+                res.sendStatus(500);
+                console.log(err.message);
+                return;
+            }
+            if(docs.length <= 0){
+                res.send('<script type="text/javascript">alert("아이디나 비밀번호가 틀렸습니다.");</script><script type="text/javascript">window.location="http://localhost:8080";</script>');
+            }else{
+                
+                res.cookie("user", docs[0].username, {    //첫번째 인자는 쿠키 이름, 두번째 인자는 값, 세번째 인자는 옵션(expire : 소멸 시간, httpOnly : 웹 서버에서만 접근 가능하게 설정)
+                    expires: new Date(Date.now() + 900000),
+                    httpOnly: true
+                });
+                req.session.islogin = 'login'
+
+                res.send('<script type="text/javascript">alert("로그인에 성공하였습니다.");</script><script type="text/javascript">window.location="http://localhost:8080";</script>');
+                console.log(req.cookies);
+                console.log('사용자 로그인');
+            } 
+        });
+});
+
+app.get('/logout', (req, res)=> {
+    req.session.islogin = ''
+    res.redirect('/')
 });
 
 var errorHandler = expressErrorHandler(
